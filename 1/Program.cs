61ab5453fbea610fcc2080b639712ce0fe07813a
AndriyHrytsko <Andriyhrytsko@gmail.com>
2024-10-03 16:04:06 +0300

    lab1+lab2

---
+++ 1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace MatrixExample
+{
+    class MainClass
+    {
+        static int[,] matrix;
+        static int[] maxElements;
+        static int M, N;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of rows (M): ");
+            M = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number of columns (N): ");
+            N = int.Parse(Console.ReadLine());
+
+            matrix = new int[M, N];
+            maxElements = new int[M];
+
+            Random random = new Random();
+
+            Console.WriteLine("Matrix:");
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = random.Next(1, 101); 
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Thread thread1 = new Thread(FindMaxInRows);
+            thread1.Start();
+            thread1.Join();  
+
+            Thread thread2 = new Thread(CalculateAverageMax);
+            thread2.Start();
+            thread2.Join();  
+        }
+
+        static void FindMaxInRows()
+        {
+            for (int i = 0; i < M; i++)
+            {
+                int max = matrix[i, 0];
+                for (int j = 1; j < N; j++)
+                {
+                    if (matrix[i, j] > max)
+                    {
+                        max = matrix[i, j];
+                    }
+                }
+                maxElements[i] = max;
+                Console.WriteLine($"Max element in row {i + 1}: {max}");
+            }
+        }
+
+        static void CalculateAverageMax()
+        {
+            double sum = 0;
+            for (int i = 0; i < M; i++)
+            {
+                sum += maxElements[i];
+            }
+            double average = sum / M;
+            Console.WriteLine($"Average of max elements: {average}");
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSync
+{
+    class Program
+    {
+        static int x = 7, y = 5, z = 4; 
+        static Semaphore S1 = new Semaphore(0, 1);
+        static Semaphore S2 = new Semaphore(0, 1); 
+        static Semaphore S3 = new Semaphore(0, 1); 
+        static Semaphore S4 = new Semaphore(0, 1); 
+
+        static void Main(string[] args)
+        {
+            Thread T1 = new Thread(() =>
+            {
+                y = y + 2;
+                Console.WriteLine("T1: y = y + 2 -> y = " + y);
+                S1.Release(); 
+            });
+
+            Thread T2 = new Thread(() =>
+            {
+                x = x * 5;
+                Console.WriteLine("T2: x = x * 5 -> x = " + x);
+                S2.Release(); 
+            });
+
+            Thread T3 = new Thread(() =>
+            {
+                z = z / 2;
+                Console.WriteLine("T3: z = z / 2 -> z = " + z);
+                S3.Release(); 
+            });
+
+            Thread T4 = new Thread(() =>
+            {
+                S2.WaitOne(); 
+                S3.WaitOne(); 
+                z = x - z;
+                Console.WriteLine("T4: z = x - z -> z = " + z);
+                S4.Release();
+            });
+
+            Thread T5 = new Thread(() =>
+            {
+                S1.WaitOne(); 
+                S4.WaitOne(); 
+                y = x * z;
+                Console.WriteLine("T5: y = x * z -> y = " + y);
+            });
+
+
+            T1.Start();
+            T2.Start();
+            T3.Start();
+            T4.Start();
+            T5.Start();
+
+            T1.Join();
+            T2.Join();
+            T3.Join();
+            T4.Join();
+            T5.Join();
+
+            Console.WriteLine("Final values: x = {0}, y = {1}, z = {2}", x, y, z);
+        }
+    }
+}
