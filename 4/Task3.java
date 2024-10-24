7b0aad356f1845ff46c71a0a6e936f55c452795d
AndriyHrytsko <Andriyhrytsko@gmail.com>
2024-10-24 15:36:50 +0300

    lad4/task3,4

--- 4/Task3.java
+++ 4/Task3.java
@@ -1,55 +1,3 @@
-class VectorInitializerX implements Runnable {
-    private final int[] X;
-
-    public VectorInitializerX(int[] X) {
-        this.X = X;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < X.length; i++) {
-            X[i] = 1; 
-        }
-        System.out.println("Ініціалізація вектора X завершена.");
-    }
-}
-
-class VectorInitializerY implements Runnable {
-    private final int[] Y;
-
-    public VectorInitializerY(int[] Y) {
-        this.Y = Y;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < Y.length; i++) {
-            Y[i] = i + 1; 
-        }
-        System.out.println("Ініціалізація вектора Y завершена.");
-    }
-}
-
-class VectorComputation implements Runnable {
-    private final int[] X;
-    private final int[] Y;
-    private final int[] Q;
-
-    public VectorComputation(int[] X, int[] Y, int[] Q) {
-        this.X = X;
-        this.Y = Y;
-        this.Q = Q;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < Q.length; i++) {
-            Q[i] = 2 * X[i] + 3 * Y[i]; 
-        }
-        System.out.println("Обчислення вектора Q завершене.");
-    }
-}
-
 public class Task3 {
     public static void main(String[] args) throws InterruptedException {
         final int N = 30; 
@@ -58,20 +6,43 @@ public class Task3 {
         int[] Y = new int[N];
         int[] Q = new int[N];
 
-        Thread threadX = new Thread(new VectorInitializerX(X));
-        Thread threadY = new Thread(new VectorInitializerY(Y));
+        // Ініціалізація вектора X
+        Thread threadX = new Thread(() -> {
+            for (int i = 0; i < X.length; i++) {
+                X[i] = 1;
+            }
+            System.out.println("Ініціалізація вектора X завершена.");
+        });
 
+        // Ініціалізація вектора Y
+        Thread threadY = new Thread(() -> {
+            for (int i = 0; i < Y.length; i++) {
+                Y[i] = i + 1;
+            }
+            System.out.println("Ініціалізація вектора Y завершена.");
+        });
+
+        // Запуск потоків ініціалізації X і Y
         threadX.start();
         threadY.start();
 
+        // Очікування завершення потоків
         threadX.join();
         threadY.join();
 
-        Thread computationThread = new Thread(new VectorComputation(X, Y, Q));
-        computationThread.start();
+        // Обчислення вектора Q
+        Thread computationThread = new Thread(() -> {
+            for (int i = 0; i < Q.length; i++) {
+                Q[i] = 2 * X[i] + 3 * Y[i];
+            }
+            System.out.println("Обчислення вектора Q завершене.");
+        });
 
+        // Запуск потоку для обчислення Q
+        computationThread.start();
         computationThread.join();
 
+        // Виведення результатів
         System.out.println("Результуючий вектор Q:");
         for (int i = 0; i < N; i++) {
             System.out.println("Q[" + i + "] = " + Q[i]);
