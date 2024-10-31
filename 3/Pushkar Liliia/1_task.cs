4359b1d420654a67ad83fc2e825588db65af818b
ililihayy <lilipushkar15@gmail.com>
2024-10-31 17:21:20 +0200

    add 3 lab

---
+++ 3/Pushkar Liliia/1_task.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace Lab3_1
+{
+
+    class _1_task
+    {
+        static EventWaitHandle wh1 = new AutoResetEvent(false),
+          wh2 = new AutoResetEvent(false),
+          wh3 = new AutoResetEvent(false);
+
+        static private int x1, x2, x3, x4, x5, x6;
+        static int A, B, C, D, result;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вираз: x1x2 + x3x4 + x5 + x6");
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            var T0 = new Thread(Func0);
+            var T1 = new Thread(Func1);
+            var T2 = new Thread(Func2);
+            var T3 = new Thread(Func3);
+            var T4 = new Thread(Func4);
+            T0.Start();
+            T1.Start();
+            T2.Start();
+            T3.Start();
+            T4.Start();
+            T4.Join();
+
+            stopwatch.Stop();
+            Console.WriteLine($"Час виконання: {stopwatch.ElapsedMilliseconds} мс");
+        }
+
+        static void Func0()
+        {
+            x1 = 1;
+            x2 = 2;
+            A = x1 * x2;
+            wh1.Set();
+        }
+
+        static void Func1()
+        {
+            x3 = 3;
+            x4 = 4;
+            B = x3 * x4;
+            wh2.Set();
+        }
+        
+        static void Func2()
+        {
+            x5 = 5;
+            x6 = 6;
+            C = x5 + x6;
+            wh3.Set();
+        }
+
+        static void Func3()
+        {
+            wh1.WaitOne();
+            wh2.WaitOne();
+            D = A + B;
+            wh3.Set();
+        }
+
+        static void Func4()
+        {
+            wh3.WaitOne();
+            result = D + C;
+            Console.WriteLine("Result = {0}", result);
+        }
+    }
+}
---
+++ 3/Pushkar Liliia/2_task.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Вираз: (M1+M2)*(M3+M4)+M5+M6");
+
+            while (true)
+            {
+                Console.Write("Введіть кількість потоків: ");
+                int P = int.Parse(Console.ReadLine());
+
+                Console.Write("Введіть розмірність матриці N (N x N): ");
+                int N = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"\nРозмірність матриці: {N}x{N}");
+                int[,] M1 = GenerateMatrix(N);
+                int[,] M2 = GenerateMatrix(N);
+                int[,] M3 = GenerateMatrix(N);
+                int[,] M4 = GenerateMatrix(N);
+                int[,] M5 = GenerateMatrix(N);
+                int[,] M6 = GenerateMatrix(N);
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
+
+                int[,] result = ParallelMatrixComputation(M1, M2, M3, M4, M5, M6, P);
+
+                stopwatch.Stop();
+                Console.WriteLine($"Час виконання: {stopwatch.ElapsedMilliseconds} мс");
+                
+                Console.Write("Бажаєте повторити процес? (y/n): ");
+                char answer = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (char.ToLower(answer) != 'y')
+                {
+                    break; 
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
+        static int[,] GenerateMatrix(int N)
+        {
+            int[,] matrix = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = random.Next(-10, 11); 
+                }
+            }
+            return matrix;
+        }
+       
+        static int[,] ParallelMatrixComputation(int[,] M1, int[,] M2, int[,] M3, int[,] M4, int[,] M5, int[,] M6, int P)
+        {
+            int N = M1.GetLength(0);
+            int[,] temp1 = new int[N, N];
+            int[,] temp2 = new int[N, N];
+            int[,] result = new int[N, N];
+
+            Parallel.For(0, N, new ParallelOptions { MaxDegreeOfParallelism = P }, i =>
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    temp1[i, j] = M1[i, j] + M2[i, j]; 
+                    temp2[i, j] = M3[i, j] + M4[i, j];
+                }
+            });
+
+            Parallel.For(0, N, new ParallelOptions { MaxDegreeOfParallelism = P }, i =>
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < N; k++)
+                    {
+                        sum += temp1[i, k] * temp2[k, j]; 
+                    }
+                    result[i, j] = sum;
+                }
+            });
+
+            Parallel.For(0, N, new ParallelOptions { MaxDegreeOfParallelism = P }, i =>
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    result[i, j] += M5[i, j] + M6[i, j]; 
+                }
+            });
+
+            return result;
+        }
+    }
+}
