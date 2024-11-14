a860f2a4ee703c1e7bb133e23bc35f071bb4819c
AndriyHrytsko <Andriyhrytsko@gmail.com>
2024-11-14 17:19:27 +0200

    lab6 done

---
+++ 6/lab6Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Linq;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        clientSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000));
+
+        byte[] buffer = new byte[1024];
+        int receivedBytes = clientSocket.Receive(buffer);
+        int numbersString = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+
+        int[] numbers = numbersString.Split(',').Select(int.Parse).ToArray();
+        Array.Sort(numbers);
+
+        string sortedNumbers = string.Join(",", numbers);
+        Console.WriteLine("Відсортований масив чисел: " + sortedNumbers);
+
+        byte[] sortedData = Encoding.UTF8.GetBytes(sortedNumbers);
+        clientSocket.Send(sortedData);
+
+        clientSocket.Close();
+    }
+}
---
+++ 6/lab6Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main(string[] args)
+    {
+        IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        serverSocket.Bind(ipEndPoint);
+        serverSocket.Listen(10);
+
+        Console.WriteLine("Очікування вхідного TCP підключення...");
+        Socket clientSocket = serverSocket.Accept();
+
+        int[] numbers = { 34, 7, 23, 32, 5, 62 };
+        Console.WriteLine("Відправлення масиву чисел клієнту: " + string.Join(", ", numbers));
+
+        byte[] data = Encoding.UTF8.GetBytes(string.Join(",", numbers));
+        clientSocket.Send(data);
+
+        byte[] buffer = new byte[1024];
+        int receivedBytes = clientSocket.Receive(buffer);
+        string sortedNumbers = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+
+        Console.WriteLine("Відсортований масив, отриманий від клієнта: " + sortedNumbers);
+
+        clientSocket.Close();
+        serverSocket.Close();
+    }
+}
